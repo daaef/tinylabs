a00a8a6e5ef51c83a59b7de8348efec6bc775f74
daaef <realafe21@gmail.com>
2020-08-09 00:45:39 +0100

    to vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,11 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: 'server',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
-  router: {
-    base: '/tinylabs/',
-  },
   head: {
     title: process.env.npm_package_name || '',
     meta: [
